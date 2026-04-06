4d55c13e904bdd719fefb56b5921395b916ff117
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-04-06 13:30:04 +0530

    add react fodler create with vite

---
+++ 01folder/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+    <>
+     <div id='dada'>
+     <div id='child1'>
+      <h1>Hello</h1>
+     </div>
+     <div id='child2'>
+      <h2>Hello vishal</h2>
+     </div>
+     
+    </div>
+    </>
+   
+  )
+}
+
+export default App
