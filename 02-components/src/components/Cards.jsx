b84da364262d2d066eac73b3fa52842485640fb2
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-04-06 16:44:50 +0530

    add components

---
+++ 02-components/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Sameer</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, quisquam.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
