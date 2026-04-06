24ba229b0df0ae3456002cce0f31e856908bce84
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-04-06 18:12:06 +0530

    add props

---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <img src='https://plus.unsplash.com/premium_photo-1764533873501-bee26e5ea0f6?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <h1>Md Sameer</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quos placeat cupiditate</p>
+        <button>View profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
