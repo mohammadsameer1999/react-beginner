52991b55326ee47fc5eeb985cff60e24506bed5a
mohammadsameer1999 <mdsameershaikh78685@gmail.com>
2026-04-15 16:51:12 +0530

    add new cards

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card/>
+      <Card user='sameer' age={25} img= 'https://plus.unsplash.com/premium_photo-1764533873501-bee26e5ea0f6?q=80&w=1015&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
 
       
     </div>
